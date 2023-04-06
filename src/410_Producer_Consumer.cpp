203f2cb8e305f0321b1b99939877ac18621e4c6c
Keith Perkins <keith.perkins@cnu.edu>
2023-04-05 21:46:10 -0400

    refactor

--- src/410_Producer_Consumer.cpp
+++ src/410_Producer_Consumer.cpp
@@ -1,97 +1,54 @@
-//============================================================================
-// Name        : 410_Producer_consumer
-// Author      : 
-// Version     :
-// Copyright   : Steal this code!
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 #include <thread>
 #include <mutex>
 #include <chrono>
 #include <condition_variable>
 #include <iostream>
-
 using namespace std;
 
-mutex m;				//mutual exclusion
-condition_variable cv; 	//for signaling
+bool done_producing = false;			//used by producer to indicate we are done
+int  gCount = 0;			//how many produced
 
-bool bDone = false;			//used by producer to indicate we are done
-int gCount = 0;
+void report(string s){
+	cout<<s<<endl;
+}
 
 void producer(int numbcounts) {
-	for (int j = 0; j < numbcounts; j++) {
-		{
-			//notice its unique
-			unique_lock<mutex> lck(m);
+	//produce numbcounts widgets
 
-			//produce something
-			gCount++;
-			cout << "Produced one, gCount=" << gCount << endl;
-		}
-		
-		//tell consumer to consume (one way comms from producer to consumer)
-		cv.notify_all();			
-	}
+	//when done set done_producing to true to leave
 
-	//one last lock to tell everyone we are done
-	{
-		unique_lock<mutex> lck(m);
-		bDone = true;
-		cout << "Producer DONE!!" << endl;
-	}
-	
-	//this will wake the consumer 
-	cv.notify_all();
+	//usefull
+	report("Produced one, gCount="+to_string(gCount));
+	report("Producer DONE, gCount="+to_string(gCount));
 }
 void consumer(int id) {
+	//if 1 produced then consume it
+	//keep consuming until time to leave
 
-	//run until we break out
-	while (true){
-		unique_lock<mutex> lck(m);
-
-		//has producer produced or is finished?
-		while (gCount==0 && !bDone)
-		{
-			cout << "   Consumer: " << id<<" waiting......." << endl;
-			cv.wait(lck);
-		}
-
-		//any work to do?
-		if (gCount>0){
-			//do it
-			gCount--;
-			cout << "   Consumer: " << id<<" consumed one, gCount=" << gCount << endl;			
-		}	
-		
-		//finished? Only when the producer is done and gCount == 0
-		if (gCount==0 && bDone){
-			break;	
-		}
-	}
-	//make sure the cout prints properly
-	unique_lock<mutex> lck(m);
-	cout << "  Consumer: " << id << " exiting" << endl;
+	//usefull
+	report("     Consumer "+to_string(id)+" consumed, gCount="+to_string(gCount));
+	report("     Consumer "+to_string(id)+" DONE, gCount="+to_string(gCount));
 }
 
 int main() {
-	cout << "The initial value of gCount is " << gCount << endl; //
+	report("The initial value of gCount is " + to_string(gCount) );
 
 	thread t_producer(producer, 1000);
+	thread t_producer1(producer, 1000);
 	thread t_consumer1(consumer, 1);
 	thread t_consumer2(consumer, 2);
-//	thread t_consumer3(consumer, 3);
-//	thread t_consumer4(consumer, 4);
+	thread t_consumer3(consumer, 3);
+	thread t_consumer4(consumer, 4);
 
 	t_producer.join();
 	t_consumer1.join();
 	t_consumer2.join();
-//	t_consumer3.join();
-//	t_consumer4.join();
+	t_consumer3.join();
+	t_consumer4.join();
+	t_producer1.join();
 
-	cout << endl << "The final value of gCount is " << gCount << endl; //
+	report("The final value of gCount is " +to_string(gCount)); //
 
 	return 0;
 }
