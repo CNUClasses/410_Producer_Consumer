d48f308449fb757eaf3902bab8f7eb2b43fb15f9
Keith Perkins <keith.perkins@cnu.edu>
2024-11-18 22:32:52 -0500

    simplified

--- src/410_Producer_Consumer.cpp
+++ src/410_Producer_Consumer.cpp
@@ -6,88 +6,68 @@
 #include <iostream>
 using namespace std;
 
-const int MAX_NUMB=5;
+mutex m;				//mutual exclusion
+mutex m_cout;			//mutual exclusion for couts
+condition_variable cv; 	//for signaling
+
+bool bDone=false;
+int numDone = 0;			//used by producer to indicate we are done
 int  gCount = 0;			//how many produced
-const int PRODUCE_THIS_MANY_THEN_WAIT=5;
-mutex m;
-mutex mcout;
-condition_variable cv;
-condition_variable cv1;
-int numprod=2;
 
 void report(string s){
-	unique_lock<mutex> lck(mcout);
+	lock_guard<mutex> lck(m_cout);
 	cout<<s<<endl;
 }
 
-void producer(int numbcounts, int id) {
-	report("     Starting producer" + to_string(id) );
-
-	//produce until done (numbcounts==0)
-	while(numbcounts>0){
+void producer(int numbcounts) {
+	for(int i=0;i<numbcounts;i++){
 		{
-			lock_guard<mutex> lck(m);
-			gCount++;
-			report("     Producer "+ to_string(id)+",produced 1,gCount="+to_string(gCount) );
-		}
-		cv.notify_one();
-
-		numbcounts--;
-
-		if(gCount==MAX_NUMB){
-			//wait until consumer says it has consumed the widget
 			unique_lock<mutex> lck(m);
-			while(gCount>0)
-				cv1.wait(lck);
+			//produce one
+			gCount++;
+			report("Produced one, gCount="+to_string(gCount));
 		}
+		cv.notify_all();
 	}
+
 	{
-		lock_guard<mutex> lck(m);
-		//indicates producer finished
-		numprod--;
+		unique_lock<mutex> lck(m);
+		//signal that we are done
+		bDone=true;
+		report("Producer DONE, gCount="+to_string(gCount));
 	}
 	cv.notify_all();
-	report("     Producer "+ to_string(id)+" leaving" );
 }
 void consumer(int id) {
-	report("Starting consumer "+to_string(id) );
-	//stay in this loop until its time to leave
 	while(true){
 		unique_lock<mutex> lck(m);
-		while(gCount==0  && (numprod>0))
+		while(gCount==0 && bDone==false)
 			cv.wait(lck);
 
-		//stay in loop until nothing else to consume and producer is done
-		if(gCount==0 && (numprod==0))
-			break;
-
+		//was 1 produced?
 		if(gCount>0){
 			gCount--;
-			report("Consumer "+to_string(id)+" consumed 1, gCount="+to_string(gCount) );
+			report("     Consumer "+to_string(id)+" consumed, gCount="+to_string(gCount));
 		}
 
-		if(gCount==0){
-			lck.unlock();
-			cv1.notify_one();
-		}
+		//is it time to leave?
+		if(gCount ==0 && bDone==true)
+			break;
 	}
-	report("leaving consumer "+to_string(id) );
+
+	report("     Consumer "+to_string(id)+" DONE, gCount="+to_string(gCount));
 }
 
 int main() {
 	report("The initial value of gCount is " + to_string(gCount) );
-	thread t_producer1(producer, 1000,1);
-	thread t_producer2(producer, 2000,2);
+
+	thread t_producer(producer, 1000);
 	thread t_consumer1(consumer, 1);
-	thread t_consumer2(consumer, 2);
-	thread t_consumer3(consumer, 3);
-	thread t_consumer4(consumer, 4);
-	t_producer1.join();
-	t_producer2.join();
+
+	t_producer.join();
 	t_consumer1.join();
-	t_consumer2.join();
-	t_consumer3.join();
-	t_consumer4.join();
+
 	report("The final value of gCount is " +to_string(gCount)); //
+
 	return 0;
 }
